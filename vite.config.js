643f47db6f92ffdd8ad9254b978ba0b53692bb33
class-Avirup <132709541+class-Avirup@users.noreply.github.com>
2024-05-14 02:09:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-export default {
-  build: {
+build: {
     rollupOptions: {
       external: ['/src/main.jsx']
     }
-  }
-}
+  }})
+
