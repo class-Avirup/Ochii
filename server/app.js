1bd302132f9a8145c7564ae31e15abb9efd8a5e5
Avirup <132709541+class-Avirup@users.noreply.github.com>
2025-05-10 17:54:49 +0530

    backend deploy

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const dotenv = require('dotenv');
 const router = require('./Router/auth-user.js');
 
 // Initialize dotenv
