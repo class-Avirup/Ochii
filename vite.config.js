fa4e38227021fa8d933404af7862b0765dc24f82
Avirup <132709541+class-Avirup@users.noreply.github.com>
2025-05-10 14:11:23 +0530

    frontend host fin

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [react()], server: {
-    historyApiFallback: true
-  },
-build: {
-    rollupOptions: {
-      external: ['/src/main.jsx']
-    },outDir: 'dist',
-    emptyOutDir: true
-  }})
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
+})
 
